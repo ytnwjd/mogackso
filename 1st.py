839715ca67d444d0a5abcab192fe1842d40a92bd
y.tnwjd <104577505+ytnwjd@users.noreply.github.com>
2023-01-18 16:29:51 +0900

    Update 1st.py
    1st_2

--- 1st.py
+++ 1st.py
@@ -17,3 +17,27 @@ def blink1():
   time.sleep(1)
   board.digitalWrite(13, "HIGH")
   time.sleep(1)
+
+#아두이노 보드와 통신하기
+# from Arduino import Arduino
+# import time
+  
+# board = Arduino('9600')
+borad.pinMode(13, "OUTPUT")
+board.pinMode(12, "OUTPUT")
+board.pinMode(11, "OUTPUT")
+  
+while True:
+   board.digitalWrite(13, "HIGH")
+   time.sleep(1)
+   board.digitalWrite(13, "LOW")
+   time.sleep(1)
+   board.digitalWrite(12, "HIGH")
+   time.sleep(1)
+   board.digitalWrite(12, "LOW")
+   time.sleep(1)
+   board.digitalWrite(11, "HIGH")
+   time.sleep(1)
+   board.digitalWrite(11, "LOW")
+   time.sleep(1)
+  
